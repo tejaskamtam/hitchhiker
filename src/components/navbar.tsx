7beac61cf62d9cf65678e2e062b6c11ed4d5c148
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2023-04-22 02:01:54 -0700

    overall css updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
- 
+import React from 'react';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
 const Navbar = () => {
-    return (
-        <nav className={styles.mainnav}>
-            <ul>
-                <Link href='/'><li>Home</li></Link>
-                <Link href='/'><li>Login</li></Link>
-                <Link href='/about'><li>About</li></Link>
-            </ul>
-        </nav>
-    )
-}
- 
-export default Navbar
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.navbar_logo_container}>
+        <img className={styles.navbar_logo} src="HitchHiker.svg"></img>
+        <p className={styles.navbar_logo_text}>HitchHiker</p>
+      </Link>
+
+      <div className={styles.navbar_tabs}>
+        <Link href="/login" className={styles.navbar_tab}>
+          Login
+        </Link>
+        <Link href="/about" className={styles.navbar_tab}>
+          About
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/test.jsx
@@ -0,0 +1,51 @@
+import styles from '@/styles/Home.module.css';
+import { Inter } from 'next/font/google';
+import { useState } from 'react';
+import Login from '../components/Login';
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  let history = [];
+  //TODO: get prompts from firebase and update history
+  const [prompts, setPrompts] = useState([]);
+  // chat with AI
+  async function onSubmit() {
+    const user_prompt = document.getElementById('user-input').value;
+    document.getElementById('user-input').value = '';
+    console.log(user_prompt);
+    history = [...prompts];
+    history.push({ role: 'user', content: user_prompt });
+
+    const response = await fetch('./api/openai', {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ prompt: user_prompt.toString(), mem: history }),
+    });
+    // of return type - {response: {role:"", content:""}}
+    const res = await response.json();
+    history.push(res.response);
+
+    if (history) {
+      setPrompts([...history]);
+    }
+    //setDoc(doc(db, "users", auth.uid), { prompts: history }, { merge: true });
+    console.log(prompts);
+  }
+
+  return (
+    <div className="container">
+      <div className="grid place-content-center">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-4xl my-8">HITCH HIKE YOUR TRIP PLANNING</h1>
+        </div>
+      </div>
+      <input type="text" id="user-input" />
+      <button id="submit-button" onClick={onSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+}
