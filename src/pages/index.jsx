2bfec2d9320d4fd8a18667ecf6491ca8af5a4325
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-04-22 00:28:55 -0700

    fixed index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/Home.module.css";
 import { Inter } from "next/font/google";
-import Login from "../components/Login";
 import { useState } from "react";
+import Login from "../components/Login";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
