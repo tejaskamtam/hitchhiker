412a9045e7cf7326a169350127588cc1453833d1
ritzz26 <ritagarwalrit@gmail.com>
2023-04-22 17:50:26 -0700

    email authentication

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import {
   getAuth,
   getRedirectResult,
   signInWithRedirect,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -18,35 +19,53 @@ const LoginPage = () => {
   const auth = getAuth();
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
+  
   if (loading) {
     return <div></div>;
   }
   if (user) {
     router.push("/");
   }
-  const SignIn = async () => {
+  const SignIn_G_Auth = async () => {
     //console.log(auth, provider);
     const result = await signInWithRedirect(auth, provider);
     //console.log(result);
     //const userCred = await getRedirectResult(auth);
     //console.log(userCred);
   };
+  const SignIn_Email = async (e) => {
+    e.preventDefault();
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+    const result = signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      //signed in 
+      const user = userCredential.user;
+    });
+    };
   return (
     <PopinBottom>
       <div className={login_styles.container}>
         <form className={login_styles.login_form}>
-          <h1 className={login_styles.login_title}>Login or Sign Up</h1>
+          <h1 className={login_styles.login_title}>Login</h1>
+          <div>
+          <h2>
+          <Link href="/signup">
+              Signup
+          </Link>
+          </h2>
+          </div>
           <div>
             <label htmlFor="">Email:</label>
             <input type="email" id="email" />
             <label htmlFor="">Password:</label>
             <input type="password" id="password" />
-            <button className={login_styles.login_button} onClick={SignIn}>
+            <button className={login_styles.login_button} onClick={SignIn_Email}>
               Submit
             </button>
           </div>
           - or -
-          <button className={login_styles.google_oauth} onClick={SignIn}>
+          <button className={login_styles.google_oauth} onClick={SignIn_G_Auth}>
             <img src="google.png" alt="google" />
             <p>Login in with Google</p>
           </button>
@@ -54,6 +73,7 @@ const LoginPage = () => {
       </div>
     </PopinBottom>
   );
+  
 };
 
 export default LoginPage;
